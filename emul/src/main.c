938abe2c7354fb93c8ed3b4f2da26be8708a8418
Ian Moffett <ian@osmora.org>
2026-01-30 19:31:35 -0500

    emu: Add initial program options
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- emul/src/main.c
+++ emul/src/main.c
@@ -4,9 +4,48 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
+
+#define VERSION "0.0.1"
+
+static void
+help(void)
+{
+    printf(
+        "---- the TPC64 emulator ----\n"
+        "[-h]   Display this help menu\n"
+        "[-v]   Display version\n"
+        "----------------------------\n"
+    );
+}
+
+static void
+version(void)
+{
+    printf(
+        "---- the TPC64 emulator ----\n"
+        "Copyright (c) 2026, Ian Moffett\n"
+        "Version %s\n"
+        "----------------------------\n",
+        VERSION
+    );
+}
 
 int
 main(int argc, char **argv)
 {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "hv")) != -1) {
+        switch (opt) {
+        case 'h':
+            help();
+            return -1;
+        case 'v':
+            version();
+            return -1;
+        }
+    }
+
     return 0;
 }
